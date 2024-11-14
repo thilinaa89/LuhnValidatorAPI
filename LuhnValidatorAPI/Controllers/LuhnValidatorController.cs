93ce8b68e5ea3623700fd862e6681af785921476
thilinaa89 <madhushani.thilina@gmail.com>
2024-11-14 17:13:32 +0530

    Add LuhnValidatorController with validation endpoint

---
+++ LuhnValidatorAPI/Controllers/LuhnValidatorController.cs
@@ -0,0 +1,30 @@
+﻿using LuhnValidatorAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LuhnValidatorAPI.Controllers
+{
+    [ApiController]
+    public class LuhnValidatorController : ControllerBase
+    {
+        private readonly ILuhnValidator _luhnValidator;
+
+        public LuhnValidatorController(ILuhnValidator luhnValidator)
+        {
+            _luhnValidator = luhnValidator;
+        }
+
+        /// <param name="cardNumber">The credit card number as a string.</param>
+        /// <returns> Returns true if the credit card number is valid.</returns>
+        [HttpGet("validate")]
+        public IActionResult ValidateCardNumber([FromQuery] string cardNumber)
+        {
+            if (string.IsNullOrWhiteSpace(cardNumber))
+            {
+                return BadRequest("Card number cannot be empty.");
+            }
+
+            bool isValid = _luhnValidator.IsValid(cardNumber);
+            return Ok(new { CardNumber = cardNumber, IsValid = isValid });
+        }
+    }
+}
